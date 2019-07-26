fc7d85f9429e3f611344d02ad2b9c667aafa4a1f
enjoycode <rick@bizdp.com>
2019-07-26 14:26:26 +0800

    fix ServiceDesigner's ReferencesDialog

--- src/components/Designers/Service/ReferencesDialog.vue
+++ src/components/Designers/Service/ReferencesDialog.vue
@@ -1,132 +1,129 @@
 <template>
-    <el-dialog class="dialog" width="800px" title="服务依赖管理" :visible.sync="visible" :close-on-click-modal="false" @close="onClose">
-        <el-transfer filterable v-model="references" :data="source" :titles="['可选项','已选项']">
+    <el-dialog class="dialog" width="800px" title="Service References" :visible.sync="visible" :close-on-click-modal="false" @close="onClose">
+        <el-transfer filterable v-model="references" :data="source" :titles="['Available','Selected']">
         </el-transfer>
         <div slot="footer">
             <input ref="input" @change="onFileChange" :multiple="false" accept=".dll,.so,.dylib" style="width:0px; visibility: hidden;"
                 type="file">
-            <el-button type="primary" @click="onUpload">上传第三方组件</el-button>
-            <el-button @click="visible = false">取 消</el-button>
-            <el-button type="primary" @click="onOkClick">确 定</el-button>
+            <el-button type="primary" @click="onUpload">Upload Assembly</el-button>
+            <el-button @click="visible = false">Cancel</el-button>
+            <el-button type="primary" @click="onOkClick">OK</el-button>
         </div>
     </el-dialog>
 </template>
 
 <script>
-    import store from '@/design/DesignStore'
-    import axios from 'axios'
+import store from '@/design/DesignStore'
+import axios from 'axios'
 
-    const sysDeps = [
-        'AppBox.Core',
-        'AppBox.Drawing',
-        'AppBox.Reporting',
-        'Cassandra',
-        'Newtonsoft.Json',
-        'System',
-        'System.Collections.NonGeneric',
-        'System.Collections.Specialized',
-        'System.ComponentModel',
-        'System.ComponentModel.Primitives',
-        'System.ComponentModel.TypeConverter',
-        'System.IO',
-        'System.Private.Xml',
-        'System.Private.Xml.Linq',
-        'System.Private.Uri',
-        'System.Net.Primitives',
-        'System.Net.Http',
-        'System.Net.Requests',
-        'System.Net.WebHeaderCollection',
-        'System.ObjectModel',
-        'System.Runtime.Extensions',
-        'System.Runtime.InteropServices',
-        'System.Security.Cryptography.Primitives',
-        'System.Security.Cryptography.Algorithms',
-        'System.Security.Cryptography.Csp',
-        'System.Security.Cryptography.X509Certificates',
-        'System.Threading.Timer',
-        'System.Web',
-        'System.Web.HttpUtility',
-        'System.Xml.Linq',
-        'System.Xml.ReaderWriter',
-        'System.Xml.Serialization',
-        'System.Xml.XmlSerializer'
-    ]
+const sysDeps = [
+    'Newtonsoft.Json',
+    'System',
+    'System.Collections.NonGeneric',
+    'System.Collections.Specialized',
+    'System.ComponentModel',
+    'System.ComponentModel.Primitives',
+    'System.ComponentModel.TypeConverter',
+    'System.IO',
+    'System.Private.Xml',
+    'System.Private.Xml.Linq',
+    'System.Private.Uri',
+    'System.Net.Primitives',
+    'System.Net.Http',
+    'System.Net.Requests',
+    'System.Net.WebHeaderCollection',
+    'System.ObjectModel',
+    'System.Runtime.Extensions',
+    'System.Runtime.InteropServices',
+    'System.Security.Cryptography.Primitives',
+    'System.Security.Cryptography.Algorithms',
+    'System.Security.Cryptography.Csp',
+    'System.Security.Cryptography.X509Certificates',
+    'System.Threading.Timer',
+    'System.Web',
+    'System.Web.HttpUtility',
+    'System.Xml.Linq',
+    'System.Xml.ReaderWriter',
+    'System.Xml.Serialization',
+    'System.Xml.XmlSerializer'
+]
 
-    export default {
-        data() {
-            return {
-                visible: true,
-                source: [],
-                references: [],
-                targetNode: null
-            }
+export default {
+    data() {
+        return {
+            visible: true,
+            source: [],
+            references: [],
+            targetNode: null
+        }
+    },
+    methods: {
+        onClose: function (e) {
+            this.$emit('close')
         },
-        methods: {
-            onClose: function (e) {
-                this.$emit('close')
-            },
-            onFileChange(ev) {
-                const files = ev.target.files
-                if (!files) return
+        onFileChange(ev) {
+            const files = ev.target.files
+            if (!files) return
 
-                var formdata = new FormData()
-                formdata.append('file', files[0])
-                let _this = this
-                axios({
-                    url: '/api/blob/sys.DesignService.ValidateServiceDeps/sys.DesignService.UploadServiceDeps/' + this.targetNode.AppID,
-                    method: 'post',
-                    data: formdata,
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-                }).then(res => {
-                    if (res.data) { // 返回true表示CLR组件
-                        // todo:判断当前列表是否已存在，不存在则添加入列表内
-                    }
-                }).catch(err => {
-                    _this.$message.error(err.response.data)
-                })
-            },
-            onOkClick() {
-                let _this = this
-                $runtime.channel.invoke('sys.DesignService.UpdateReferences', [this.targetNode.ID, this.references]).catch(err => {
-                    _this.$message.error('更新服务依赖失败: ' + err)
-                })
-            },
-            onUpload() {
-                this.$refs.input.value = null
-                this.$refs.input.click()
-            }
-        },
-        mounted() {
-            for (var i = 0; i < sysDeps.length; i++) {
-                this.source.push({ key: sysDeps[i], label: sysDeps[i] })
-            }
-            // 加载Application的依赖项
-            let designer = store.designers.getActiveDesigner()
-            this.targetNode = designer.target
-            var _this = this
-            $runtime.channel.invoke('sys.DesignService.GetReferences', [this.targetNode.ID]).then(res => {
-                let d = JSON.parse(res)
-                if (d.AppDeps) {
-                    for (var i = 0; i < d.AppDeps.length; i++) {
-                        var element = d.AppDeps[i]
-                        _this.source.push({ key: element, label: element, ext: true }) // 加入ext表示外部组件
-                    }
-                }
-                if (d.ModelDeps) {
-                    for (i = 0; i < d.ModelDeps.length; i++) {
-                        _this.references.push(d.ModelDeps[i])
-                    }
+            var formdata = new FormData()
+            formdata.append('file', files[0])
+            let _this = this
+            axios({
+                url: '/api/blob/sys.DesignService.ValidateServiceDeps/sys.DesignService.UploadServiceDeps/' + this.targetNode.AppID,
+                method: 'post',
+                data: formdata,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).then(res => {
+                if (res.data) { // 返回true表示CLR组件
+                    // todo:判断当前列表是否已存在，不存在则添加入列表内
                 }
             }).catch(err => {
-                _this.$message.error('加载服务依赖失败: ' + err)
+                _this.$message.error(err.response.data)
             })
+        },
+        onOkClick() {
+            let _this = this
+            $runtime.channel.invoke('sys.DesignService.UpdateReferences', [this.targetNode.ID, this.references])
+                .then(res => { _this.visible = false })
+                .catch(err => {
+                    _this.$message.error('Update reference error: ' + err)
+                })
+        },
+        onUpload() {
+            this.$refs.input.value = null
+            this.$refs.input.click()
         }
+    },
+    mounted() {
+        for (var i = 0; i < sysDeps.length; i++) {
+            this.source.push({ key: sysDeps[i], label: sysDeps[i] })
+        }
+        // 加载Application的依赖项
+        let designer = store.designers.getActiveDesigner()
+        this.targetNode = designer.target
+        var _this = this
+        $runtime.channel.invoke('sys.DesignService.GetReferences', [this.targetNode.ID]).then(res => {
+            if (res.AppDeps) {
+                for (var i = 0; i < res.AppDeps.length; i++) {
+                    var element = res.AppDeps[i]
+                    _this.source.push({ key: element, label: element, ext: true }) // 加入ext表示外部组件
+                }
+            }
+            if (res.ModelDeps) {
+                for (i = 0; i < res.ModelDeps.length; i++) {
+                    _this.references.push(res.ModelDeps[i])
+                }
+            }
+        }).catch(err => {
+            _this.$message.error('Load references error: ' + err)
+        })
     }
+}
 
 </script>
 
 <style scoped>
-    .dialog>>>.el-transfer-panel {
-        width: 330px;
-    }
+.dialog >>> .el-transfer-panel {
+    width: 330px;
+}
 </style>
