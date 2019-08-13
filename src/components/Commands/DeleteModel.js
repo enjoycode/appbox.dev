b27e26be79a49db697afad6e8b21895c4fddceb9
enjoycode <rick@bizdp.com>
2019-08-13 11:15:37 +0800

    delete node

--- src/components/Commands/DeleteModel.js
+++ src/components/Commands/DeleteModel.js
@@ -6,27 +6,27 @@ import store from '@/design/DesignStore'
 export default function () {
     var selectNode = store.tree.currentNode
     if (!selectNode) {
-        Message.warning('请选择要删除的节点')
+        Message.warning('Please select node first.')
         return false
     }
     var nodeType = selectNode.Type
     // FolderNode = 6
     if ((nodeType < 6 && nodeType !== 3) || (nodeType === 6 && selectNode.Nodes.length > 0)) {
-        Message.warning('不允许删除此节点')
+        Message.warning('Can not delete this node.')
         return false
     }
-    MessageBox.confirm('确定删除选中的节点吗？', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
+    MessageBox.confirm('Are you sure to delete this node', 'Confirm', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
         type: 'warning'
     }).then(() => {
         // 获取实体属性
-        $runtime.channel.invoke('sys.DesignService.Delete', [selectNode.Type, selectNode.ID]).then(res => {
+        $runtime.channel.invoke('sys.DesignService.DeleteNode', [selectNode.Type, selectNode.ID]).then(res => {
             // 移除选中节点
             store.tree.onDeleteNode(selectNode, res)
             // 移除选中节点对应的tab
             store.designers.removeTabByNode(selectNode)
-            Message.success('删除成功')
+            Message.success('Delete succeed')
         }).catch(err => {
             Message.error(err)
         })
