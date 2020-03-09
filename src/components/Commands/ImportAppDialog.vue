d3eae14a909f57e9ca87dfc71e5d9db50ce9e934
enjoycode <enjoycode@icloud.com>
2020-03-09 11:18:38 +0800

    reload design tree after import app package.

--- src/components/Commands/ImportAppDialog.vue
+++ src/components/Commands/ImportAppDialog.vue
@@ -1,6 +1,6 @@
 <template>
     <el-dialog title="Import Application" :visible.sync="visible" :close-on-click-modal="false" @close="onClose" width="400px">
-        <el-upload drag action="/api/design/import" :multiple="false">
+        <el-upload drag action="/api/design/import" on-success="onDone" :multiple="false">
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">Drag package file here ，or <em>click to upload</em></div>
         </el-upload>
@@ -15,6 +15,9 @@ export default {
         return { visible: true }
     },
     methods: {
+        onDone: function (response, file, fileList) {
+            store.tree.loadTree() //成功导入刷新设计树
+        },
         onClose: function (e) {
             this.$emit('close')
         }
