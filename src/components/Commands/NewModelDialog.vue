8c9cd64a8449e848cb18117cf3424ce83552ee1b
enjoycode <enjoycode@icloud.com>
2021-03-13 12:43:48 +0800

    新建视图模型时可以选择视图类型.

--- src/components/Commands/NewModelDialog.vue
+++ src/components/Commands/NewModelDialog.vue
@@ -2,11 +2,15 @@
     <el-dialog :title="title" width="500px" :visible.sync="visible" :close-on-click-modal="false" @close="onClose">
         <el-form :model="model" ref="model" :rules="rules" label-width="120px" label-position="right">
             <el-form-item prop="Name" :required="true" label="Name">
-                <el-input v-model="model.Name" ></el-input>
+                <el-input v-model="model.Name"></el-input>
             </el-form-item>
             <!--<el-form-item prop="LocalizedName" label="本地化名称">
                 <el-input v-model="model.LocalizedName" ></el-input>
             </el-form-item>-->
+            <el-form-item prop="Type" label="Type" v-if="dlgProps === 'View'">
+                <el-radio v-model="model.Type" :label="0">Vue Code</el-radio>
+                <el-radio v-model="model.Type" :label="1">Vue Visual</el-radio>
+            </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
             <el-button :disabled="caDisabled" @click="visible = false">Cancel</el-button>
@@ -17,7 +21,7 @@
 
 <script>
 import store from '@/design/DesignStore'
-import { modelLibs } from '../CodeEditor/EditorService'
+import {modelLibs} from '../CodeEditor/EditorService'
 
 export default {
     props: ['dlgProps'], // modelType: string
@@ -38,10 +42,10 @@ export default {
             visible: true,
             okDisabled: false, // ok按钮是否禁用
             caDisabled: false, // cancel按钮是否禁用
-            model: { Name: '' },
+            model: {Name: '', Type: 0}, //Type暂仅用于ViewModel
             rules: {
                 Name: [
-                    { validator: this.validateName, trigger: 'change' }
+                    {validator: this.validateName, trigger: 'change'}
                 ]
             }
         }
@@ -52,15 +56,19 @@ export default {
         },
         submit: function (formName) {
             this.$refs[formName].validate((valid) => {
-                if (!valid) { return false }
+                if (!valid) {
+                    return false
+                }
                 this.okDisabled = true
                 this.caDisabled = true
 
-                var node = store.tree.currentNode
-                var _this = this
-                var args = []
+                const node = store.tree.currentNode;
+                const _this = this;
+                let args = [];
                 if (this.dlgProps === 'Application') {
                     args = [this.model.Name]
+                } else if (this.dlgProps === 'View') {
+                    args = [node.Type, node.ID, this.model.Name, this.model.Type]
                 } else {
                     args = [node.Type, node.ID, this.model.Name]
                 }
@@ -68,7 +76,7 @@ export default {
                 $runtime.channel.invoke(this.service, args).then(res => {
                     // 根据返回结果添加新节点
                     store.tree.onNewNode(res)
-                    if (this.props === 'View') { // 如果新建视图添加ts声明代码
+                    if (this.dlgProps === 'View') { // 如果新建视图添加ts声明代码
                         modelLibs.addView(res.NewNode)
                     }
                     _this.$message.success('Create ' + this.dlgProps + 'succeed.')
