e0adaf397df3283ea1f749b163cd65d76edd5cae
enjoycode <enjoycode@icloud.com>
2021-03-16 16:36:26 +0800

    fix close RouteDialog.vue

--- src/components/Designers/View/RouteDialog.vue
+++ src/components/Designers/View/RouteDialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-dialog title="Route" width="500px" :visible.sync="visible">
+    <el-dialog title="Route" width="500px" :visible="visible" @close="onClose">
         <el-form label-width="120px">
             <el-form-item>
                 <el-checkbox v-model="route.Enable" :disabled="readonly">List in route</el-checkbox>
@@ -19,9 +19,9 @@
             </el-form-item> -->
         </el-form>
         <span slot="footer" class="dialog-footer">
-                        <el-button @click="$emit('update:visible', false)">Cancel</el-button>
-                        <el-button :disabled="readonly" type="primary" @click="onChange">OK</el-button>
-                    </span>
+            <el-button @click="onClose">Cancel</el-button>
+            <el-button :disabled="readonly" type="primary" @click="onChange">OK</el-button>
+        </span>
     </el-dialog>
 </template>
 
@@ -35,6 +35,10 @@ export default class RouteDialog extends Vue {
     @Prop({type: Boolean, required: true}) readonly: boolean;
     @Prop({type: Boolean, required: true}) visible: boolean;
     @Prop({type: Object, required: true}) route: object;
+
+    onClose() {
+        this.$emit('update:visible', false);
+    }
 
     onChange() {
         this.$emit('change');
