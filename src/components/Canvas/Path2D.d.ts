adde0bac2c6348526623ee00189c2a36b5212f90
enjoycode <enjoycode@icloud.com>
2021-03-29 10:11:29 +0800

    remove unused.

--- src/components/Canvas/Path2D.d.ts
+++
@@ -1,9 +0,0 @@
-// // Extend CanvasRenderingContext2D
-// interface CanvasRenderingContext2D {
-//     fill(): void; // TODO: check it?
-//     fill(path: Path2D): void;
-//     // stroke(path: Path2D): void;
-//     // clip(path: Path2D, fillRule?: string): void;
-//     isPointInPath(path: Path2D, x: number, y: number): boolean;
-//     isPointInStroke(path: Path2D, x: number, y: number): boolean;
-// }
